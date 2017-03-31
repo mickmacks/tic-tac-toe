bb0d0c343bfc3eb946419dff673c269c14ba994b
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-03-31 16:25:59 -0700

    Working tree fully functional. Project begins.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
+
+	console.log('JS is working! Please continue...')
+	
   // all code to manipulate the DOM
   // goes inside this function
 
