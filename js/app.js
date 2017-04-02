14cdc173d5bba525369703d74d002e032aef8f96
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-04-02 08:45:39 -0700

    Created board styles, added reset button, about to start JS

--- js/app.js
+++ js/app.js
@@ -7,3 +7,43 @@ $(document).ready(function() {
   // goes inside this function
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Player X Wins Modal (For later)
+
+      	// Get the modal
+			// var modal = document.getElementById('myModalSuccess');
+			// modal.style.display = "block";
+
+			// // Get the <span> element that closes the modal
+			// var span = document.getElementById('closeSuccess');
+
+			// // When the user clicks on <span> (x), close the modal
+			// span.onclick = function() {
+			//     modal.style.display = "none";
+			//     resetCards();
+			// }
+			// // When the user clicks anywhere outside of the modal, close it
+			// window.onclick = function(event) {
+			//     if (event.target == modal) {
+			//         modal.style.display = "none";
+			//         resetCards();
+	  		//		}
+			// }
