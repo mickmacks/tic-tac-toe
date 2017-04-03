6dd2059407cb4fcfbc0f6bf077cbed43eab58959
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-04-02 22:20:41 -0700

    Modal functionality working.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var turn = 0;
+var winner = -1;
 
 // wait for the DOM to finish loading
 $(document).ready(function() {
@@ -6,9 +7,6 @@ $(document).ready(function() {
 	console.log('JS is working! Please continue...');
 	
   // all code to manipulate the DOM goes inside this function
-
-  // Create a variable to track the current turn.
-  // Use that variable to inform the user which player's turn it is.
 
 	// Create a js version of each tile.
 	// Give each tile a value to track if filled (X), filled (O), or empty.
@@ -37,14 +35,25 @@ $(document).ready(function() {
 	console.log('tile vars added. no errors yet!');
 
 
-  // Check to see where the user clicked and then 
-  // change the css and innerHTML of the corresponding div.
+	// Check to see where the user clicked and then run a function to update that tile and relevant values.
 
- document.getElementById('tile1').onclick = changeTile1;
+	document.getElementById('tile1').onclick = changeTile;
+	document.getElementById('tile2').onclick = changeTile;
+	document.getElementById('tile3').onclick = changeTile;
+	document.getElementById('tile4').onclick = changeTile;
+	document.getElementById('tile5').onclick = changeTile;
+	document.getElementById('tile6').onclick = changeTile;
+	document.getElementById('tile7').onclick = changeTile;
+	document.getElementById('tile8').onclick = changeTile;
+	document.getElementById('tile9').onclick = changeTile;
 
-  function changeTile1() {
+	// Create the function to run when called.
 
-  	if (turn % 2 === 0 && tile1 === 0) {
+	function changeTile() {
+
+		// Check if the corresponding value of the clicked tile is 0
+
+  		if (turn % 2 === 0 && tile1 === 0) {
 
   		// Change the class of tile 1 to display an X
   		document.getElementById('tile1').innerHTML = '<i class="medium material-icons valign center-block">grade</i>'
@@ -61,6 +70,11 @@ $(document).ready(function() {
 
   		// Add 1 to the turn counter.
   		turn++;
+
+  		// Check for winner
+  		checkForWinner();
+
+  		console.log('checked for winner');
 
 
   	} else if (turn % 2 !== 0 && tile1 === 0) {
@@ -81,53 +95,8 @@ $(document).ready(function() {
   		// Add 1 to the turn counter.
   		turn++;
 
-  	} else {
-
-  		alert('This spot is taken :(');
-  	}
-  	
-  }
-
- document.getElementById('tile2').onclick = changeTile2;
-
-  function changeTile2() {
-
-  	if (turn % 2 === 0 && tile2 === 0) {
-
-  		// Change the class of tile 1 to display an X
-  		document.getElementById('tile2').innerHTML = '<i class="medium material-icons valign center-block">grade</i>'
-
-  		// set the new class
-
-  		document.getElementById('tile2').className = 'player1 col m4 valign-wrapper box';
-
-  		// change player turn
-  		document.getElementById('turnCounter').innerHTML = "visibility";
-
-  		// Update the value of this tile to 1.
-  		tile2 += 1;
-
-  		// Add 1 to the turn counter.
-  		turn++;
-
-
-  	} else if (turn % 2 !== 0 && tile2 === 0) {
-
-  		// Change the class of tile 1 to display an O
-  		document.getElementById('tile2').innerHTML = '<i class="medium material-icons valign center-block">visibility</i>'
-
-  		// set the new class
-
-  		document.getElementById('tile2').className = 'player2 col m4 valign-wrapper box';
-
-  		// change player turn
-  		document.getElementById('turnCounter').innerHTML = "grade";
-
-  		// Update the value of this tile to 1.
-  		tile2 += 2;
-
-  		// Add 1 to the turn counter.
-  		turn++;
+   		// Check for winner
+  		checkForWinner();
 
   	} else {
 
@@ -136,192 +105,166 @@ $(document).ready(function() {
   	
   }
 
-  // 	var gameBoard = document.getElementById('board');
-  // 	gameBoard.addEventListener('click', changeTile);
+ // document.getElementById('tile2').onclick = changeTile2;
 
-  // 	function changeTile1() {
+ //  function changeTile2() {
 
-  // 		if (turn % 2 === 0 && // tile === 0) {
+ //  	if (turn % 2 === 0 && tile2 === 0) {
 
-		// 	// change the class of clicked tile to player1
-		// 	tile1
+ //  		// Change the class of tile 1 to display an X
+ //  		document.getElementById('tile2').innerHTML = '<i class="medium material-icons valign center-block">grade</i>'
 
-		// 	// change the icon to X after click
-		// 	querySelector('player1').innerHTML = grade;
+ //  		// set the new class
 
-		// 	// add 1 to the turn counter
-		// 	turn += 1;
+ //  		document.getElementById('tile2').className = 'player1 col m4 valign-wrapper box';
 
-		// 	// change the value of clicked tile to 1
+ //  		// change player turn
+ //  		document.getElementById('turnCounter').innerHTML = "visibility";
 
+ //  		// Update the value of this tile to 1.
+ //  		tile2 += 1;
 
-
-		// } else if (turn % 2 !== 0 && // tile === 0) {
-		
-		// 	// change the class of clicked tile to player2
-
-		// 	// change the icon to O after click
-		// 	querySelector('player1').innerHTML = visibility;
-
-		// 	// add 1 to the turn counter
-		// 	turn += 2;
-			
-		// 	// change the value of clicked tile to 2
-
-		// } else {
-
-		// }
+ //  		// Add 1 to the turn counter.
+ //  		turn++;
 
 
-  // 	}
+ //  	} else if (turn % 2 !== 0 && tile2 === 0) {
+
+ //  		// Change the class of tile 1 to display an O
+ //  		document.getElementById('tile2').innerHTML = '<i class="medium material-icons valign center-block">visibility</i>'
+
+ //  		// set the new class
+
+ //  		document.getElementById('tile2').className = 'player2 col m4 valign-wrapper box';
+
+ //  		// change player turn
+ //  		document.getElementById('turnCounter').innerHTML = "grade";
+
+ //  		// Update the value of this tile to 1.
+ //  		tile2 += 2;
+
+ //  		// Add 1 to the turn counter.
+ //  		turn++;
+
+ //  	} else {
+
+ //  		alert('This spot is taken :(');
+ //  	}
+  	
+ //  }
 
 
   // 5. Check to see whether three Xs or Os line up using if statements with && or ||
   // 6. Add a condition to check for a draw if 9 turns have been played.
   // 7. When the if conditions are met, alert which user has won or if there's a draw.
 
-  	if (tile1 === 1 && tile2 === 1 && tile3 === 1 || 
-  		tile4 === 1 && tile5 === 1 && tile6 === 1 || 
-  		tile7 === 1 && tile8 === 1 && tile9 === 1 || 
-  		tile1 === 1 && tile4 === 1 && tile7 === 1 || 
-  		tile2 === 1 && tile5 === 1 && tile8 === 1 || 
-  		tile3 === 1 && tile6 === 1 && tile9 === 1 || 
-  		tile1 === 1 && tile5 === 1 && tile9 === 1 || 
-  		tile3 === 1 && tile5 === 1 && tile7 === 1 ) {
+  	function checkForWinner() {
 
-  		// Player X Wins Modal (pulled from memory game)
+	  	if (tile1 === 1 && tile2 === 1 && tile3 === 1 || 
+	  		tile4 === 1 && tile5 === 1 && tile6 === 1 || 
+	  		tile7 === 1 && tile8 === 1 && tile9 === 1 || 
+	  		tile1 === 1 && tile4 === 1 && tile7 === 1 || 
+	  		tile2 === 1 && tile5 === 1 && tile8 === 1 || 
+	  		tile3 === 1 && tile6 === 1 && tile9 === 1 || 
+	  		tile1 === 1 && tile5 === 1 && tile9 === 1 || 
+	  		tile3 === 1 && tile5 === 1 && tile7 === 1 ) {
 
-      	// Get the modal
-      	
-			var modal = document.getElementById('myModalSuccess');
-			modal.style.display = "block";
+	  		// Player X Wins Modal (pulled from memory game)
 
-			// Get the <span> element that closes the modal
-			var span = document.getElementById('closeSuccess');
+	      	// Get the modal
 
-			// When the user clicks on <span> (x), close the modal
-			span.onclick = function() {
-			    modal.style.display = "none";
-			    resetCards();
-			}
-			// When the user clicks anywhere outside of the modal, close it
-			window.onclick = function(event) {
-			    if (event.target == modal) {
-			        modal.style.display = "none";
-			        resetCards();
-	  				}
-			}
+	      		var winner = 0;
+	      	
+				var modal = document.getElementById('player1Wins');
+				modal.style.display = "block";
 
-  	} else 
+				// Get the <span> element that closes the modal
+				var span = document.getElementById('player1Close');
 
-  	if (tile1 === 2 && tile2 === 2 && tile3 === 2 || 
-  		tile4 === 2 && tile5 === 2 && tile6 === 2 || 
-  		tile7 === 2 && tile8 === 2 && tile9 === 2 || 
-  		tile1 === 2 && tile4 === 2 && tile7 === 2 || 
-  		tile2 === 2 && tile5 === 2 && tile8 === 2 || 
-  		tile3 === 2 && tile6 === 2 && tile9 === 2 || 
-  		tile1 === 2 && tile5 === 2 && tile9 === 2 || 
-  		tile3 === 2 && tile5 === 2 && tile7 === 2 ) {
+				// When the user clicks on <span> (x), close the modal
+				span.onclick = function() {
+				    modal.style.display = "none";
+				    window.location.reload(true);
+				}
+				// When the user clicks anywhere outside of the modal, close it
+				window.onclick = function(event) {
+				    if (event.target == modal) {
+				        modal.style.display = "none";
+				        window.location.reload(true);
+		  				}
+				}
 
-  		// Player O Wins Modal
+	  	} else 
 
-      	// Get the modal
-      	
-			var modal = document.getElementById('myModalSuccess');
-			modal.style.display = "block";
+	  	if (tile1 === 2 && tile2 === 2 && tile3 === 2 || 
+	  		tile4 === 2 && tile5 === 2 && tile6 === 2 || 
+	  		tile7 === 2 && tile8 === 2 && tile9 === 2 || 
+	  		tile1 === 2 && tile4 === 2 && tile7 === 2 || 
+	  		tile2 === 2 && tile5 === 2 && tile8 === 2 || 
+	  		tile3 === 2 && tile6 === 2 && tile9 === 2 || 
+	  		tile1 === 2 && tile5 === 2 && tile9 === 2 || 
+	  		tile3 === 2 && tile5 === 2 && tile7 === 2 ) {
 
-			// Get the <span> element that closes the modal
-			var span = document.getElementById('closeSuccess');
+	  		// Player O Wins Modal
 
-			// When the user clicks on <span> (x), close the modal
-			span.onclick = function() {
-			    modal.style.display = "none";
-			    resetCards();
-			}
-			// When the user clicks anywhere outside of the modal, close it
-			window.onclick = function(event) {
-			    if (event.target == modal) {
-			        modal.style.display = "none";
-			        resetCards();
-	  				}
-			}
-  	
-  	} else if (turn === 9) {
+	  		var winner = 1;
 
-  		// Draw Modal
+	      	// Get the modal
+	      	
+				var modal = document.getElementById('player2Wins');
+				modal.style.display = "block";
 
-      	// Get the modal
-      	
-			var modal = document.getElementById('myModalSuccess');
-			modal.style.display = "block";
+				// Get the <span> element that closes the modal
+				var span = document.getElementById('player2Close');
 
-			// Get the <span> element that closes the modal
-			var span = document.getElementById('closeSuccess');
+				// When the user clicks on <span> (x), close the modal
+				span.onclick = function() {
+				    modal.style.display = "none";
+				    window.location.reload(true);
+				}
+				// When the user clicks anywhere outside of the modal, close it
+				window.onclick = function(event) {
+				    if (event.target == modal) {
+				        modal.style.display = "none";
+				        window.location.reload(true);
+		  				}
+				}
+	  	
+	  	} else if (turn === 9 && winner != 0 && winner != 1) {
 
-			// When the user clicks on <span> (x), close the modal
-			span.onclick = function() {
-			    modal.style.display = "none";
-			    resetCards();
-			}
-			// When the user clicks anywhere outside of the modal, close it
-			window.onclick = function(event) {
-			    if (event.target == modal) {
-			        modal.style.display = "none";
-			        resetCards();
-	  				}
-			}
+	  		// Draw Modal
+
+	      	// Get the modal
+	      	
+				var modal = document.getElementById('draw');
+				modal.style.display = "block";
+
+				// Get the <span> element that closes the modal
+				var span = document.getElementById('drawClose');
+
+				// When the user clicks on <span> (x), close the modal
+				span.onclick = function() {
+				    modal.style.display = "none";
+				    window.location.reload(true);
+				}
+				// When the user clicks anywhere outside of the modal, close it
+				window.onclick = function(event) {
+				    if (event.target == modal) {
+				        modal.style.display = "none";
+				        window.location.reload(true);
+		  				}
+				}
+	  	}
   	}
 
-  // 8. Use the reset button to refresh the board.
+    // 8. Use the reset button to refresh the board.
 
-  document.getElementById('reset').addEventListener('click', resetBoard);
+  	document.getElementById('reset').addEventListener('click', resetBoard);
 
-  function resetBoard() {
+  	function resetBoard() {
 
   	window.location.reload(true);
 
-  }
+  	}
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		// // Player X Wins Modal from memory card game (For later)
-
-  //     	// Get the modal
-
-		// 	var modal = document.getElementById('myModalSuccess');
-		// 	modal.style.display = "block";
-
-		// 	// Get the <span> element that closes the modal
-		// 	var span = document.getElementById('closeSuccess');
-
-		// 	// When the user clicks on <span> (x), close the modal
-		// 	span.onclick = function() {
-		// 	    modal.style.display = "none";
-		// 	    resetCards();
-		// 	}
-		// 	// When the user clicks anywhere outside of the modal, close it
-		// 	window.onclick = function(event) {
-		// 	    if (event.target == modal) {
-		// 	        modal.style.display = "none";
-		// 	        resetCards();
-	 //  				}
-		// 	}
